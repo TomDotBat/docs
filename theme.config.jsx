db6c70552c3b38c3229614732152c7013d56e3a9
Tom O'Sullivan <tom@tomdotbat.dev>
2023-06-10 13:54:48 +0100

    chore: nextra project setup

---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+	logo: <strong>Tom.docs</strong>,
+	project: {
+		link: "https://github.com/TomDotBat/docs"
+	},
+	docsRepositoryBase: "https://github.com/TomDotBat/docs/tree/master",
+	editLink: {
+		text: "Edit this page →"
+	},
+	feedback: {
+		content: "Create an issue →",
+		labels: "feedback"
+	},
+}
