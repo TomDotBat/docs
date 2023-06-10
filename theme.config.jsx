5b8db1c41df543cd7566d4aa69ecbc5154c7130a
Tom O'Sullivan <tom@tomdotbat.dev>
2023-06-10 19:05:08 +0100

    fix: head overrides

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,6 @@
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs/.";
+
 export default {
 	logo: <strong>Tom.docs</strong>,
 	project: {
@@ -13,16 +16,26 @@ export default {
 	},
 	useNextSeoProps() {
 		return {
-			titleTemplate: "%s – Tom.docs",
-			description: "The documentation for Tom's projects.",
-			openGraph: {
-				siteName: "Tom.docs",
-				type: "website",
-				locale: "en_GB",
-				url: "https://docs.tomdotbat.dev",
-				description: "The documentation for Tom's projects."
-			}
+			titleTemplate: "%s – Tom.docs"
 		};
+	},
+	head() {
+		const { asPath, defaultLocale, locale } = useRouter();
+		const { frontMatter } = useConfig();
+
+		const url = "https://docs.tomdotbat.dev"
+			+ (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+		return (
+			<>
+				<meta property="og:url" content={url} />
+				<meta property="og:title" content={frontMatter.title || "Unknown"} />
+				<meta property="og:description" content={frontMatter.description || "The documentation for Tom's projects."} />
+				<meta property="og:locale" content={locale} />
+				<meta property="og:type" content="website" />
+				<meta property="og:site_name" content="Tom.docs" />
+			</>
+		);
 	},
 	footer: {
 		text: (
