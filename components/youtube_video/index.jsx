c3bc8fda6d2e4857c9e5f804104abe1f406547cc
Tom O'Sullivan <tom@tomdotbat.dev>
2023-06-10 13:58:21 +0100

    fix: frame border property warning

--- components/youtube_video/index.jsx
+++ components/youtube_video/index.jsx
@@ -5,7 +5,7 @@ export default function YouTubeVideo({ id, title }) {
 				src={`https://www.youtube.com/embed/${id}`}
 				className="w-full aspect-video"
 				title={title}
-				frameborder="0"
+				frameBorder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 				allowfullscreen
 			>
