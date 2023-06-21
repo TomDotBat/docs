6e596f51bd7a85278687b88a3858f03e963cfa64
Gravy59 <magiccoder59@gmail.com>
2023-06-21 10:56:22 -0600

    feat: move avatar into `elements` folder

--- components/realm/index.jsx
+++ components/realm/index.jsx
@@ -1,47 +1,45 @@
 import Link from "next/link";
 
 export default function Realm({ type, size = 4 }) {
-	let background, name;
+  let background, name;
 
-	switch (type) {
-		case "client":
-			background = "#dea909";
-			name = "Client";
-			break;
-		case "server":
-			background = "#03a9f4";
-			name = "Server";
-			break;
-		case "menu":
-			background = "#4caf50";
-			name = "Menu";
-			break;
-		case "shared":
-			background = "linear-gradient(45deg, #dea909 53%, #03a9f4 53.001%)";
-			name = "Client & Server";
-			break;
-		case "shared-menu":
-			background = "linear-gradient(45deg, #dea909 53%, #4caf50 53.001%)";
-			name = "Client & Menu";
-			break;
-		case "all":
-			background = "linear-gradient(45deg, #4caf50 35.99%, #dea909 36%, #dea909 63%, #03a9f4 63.01%)";
-			name = "All Realms";
-			break;
-		default:
-			return <p>Invalid realm</p>;
-	}
+  switch (type) {
+    case "client":
+      background = "#dea909";
+      name = "Client";
+      break;
+    case "server":
+      background = "#03a9f4";
+      name = "Server";
+      break;
+    case "menu":
+      background = "#4caf50";
+      name = "Menu";
+      break;
+    case "shared":
+      background = "linear-gradient(45deg, #dea909 53%, #03a9f4 53.001%)";
+      name = "Client & Server";
+      break;
+    case "all":
+      background =
+        "linear-gradient(45deg, #4caf50 35.99%, #dea909 36%, #dea909 63%, #03a9f4 63.01%)";
+      name = "All Realms";
+      break;
+    default:
+      return <p>Invalid realm</p>;
+  }
 
-	return (
-		<Link
-			className={`${size} aspect-square block`}
-			href="https://wiki.facepunch.com/gmod/States" target="_blank"
-			title={name}
-		>
-			<div
-				className="w-full h-full rounded"
-				style={{ background: background }}
-			/>
-		</Link>
-	);
+  return (
+    <Link
+      className={`${size} aspect-square block`}
+      href="https://wiki.facepunch.com/gmod/States"
+      target="_blank"
+      title={name}
+    >
+      <div
+        className="w-full h-full rounded"
+        style={{ background: background }}
+      />
+    </Link>
+  );
 }
